a6d4bb2f2dd7fbb962ce142484296d65507382ea
DavidMukhiya <sherlock.41v94@gmail.com>
2022-05-06 00:23:38 -0700

    Created Three counter which add different range of number

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Counter from './component/counter/Counter';
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <Counter by={1} />
+      <Counter by={5} />
+      <Counter by={10} />
     </div>
   );
 }
